79d49e40b1acb948cd11108dea2e54a693759de4
btzx <benjamin.munoz.t@usach.cl>
2019-02-10 00:10:20 -0300

    archivos actualizados

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,33 +12,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(userSeeder::class);
-       
-        
         $this->call(destinySeeder::class);
         $this->call(originSeeder::class);
-         $this->call(reservationSeeder::class);
+        $this->call(reservationSeeder::class);
         $this->call(packageSeeder::class);   
         $this->call(flightSeeder::class);
         $this->call(seatSeeder::class);
         $this->call(airportSeeder::class);
         $this->call(stopSeeder::class);
         $this->call(passengerSeeder::class);
-        $this->call(socioSeeder::class);
         $this->call(check_inSeeder::class);
-       
-         $this->call(purchaseSeeder::class);
+        $this->call(purchaseSeeder::class);
         $this->call(carSeeder::class);
         $this->call(hotelSeeder::class);
         $this->call(roomSeeder::class);
-
-        $this->call(hotel_reservationSeeder::class);
         $this->call(ticketSeeder::class);
         $this->call(roomSeeder::class);
-        $this->call(stopflightSeeder::class);
-        $this->call(reservationflightSeeder::class);
-        $this->call(hotelreservationpackageSeeder::class);
-        $this->call(flightpackageSeeder::class);
-        
-
     }
 }
