078858c728b0c68019b391e642d2b6e546d444f7
btzx <benjamin.munoz.t@usach.cl>
2019-02-24 22:56:17 -0300

    navbar actualizada

--- resources/views/rooms/principal.blade.php
+++ resources/views/rooms/principal.blade.php
@@ -41,11 +41,12 @@
         <div class="collapse navbar-collapse" id="ftco-nav">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
-            <li class="nav-item"><a href="/flights" class="nav-link">Vuelos</a></li>
-            <li class="nav-item active"><a href="/rooms" class="nav-link">Habitaciones</a></li>
+            <li class="nav-item active"><a href="/flights" class="nav-link">Vuelos</a></li>
+            <li class="nav-item"><a href="/destinies" class="nav-link">Habitaciones</a></li>
             <li class="nav-item"><a href="/cars" class="nav-link">Autos</a></li>
             <li class="nav-item"><a href="/packages" class="nav-link">Paquetes</a></li>
-            <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+            <li class="nav-item"><a href="/login" class="nav-link">Ingresar</a></li>
+            <li class="nav-item"><a href="/register" class="nav-link">Registrarse</a></li>
           </ul>
         </div>
       </div>
